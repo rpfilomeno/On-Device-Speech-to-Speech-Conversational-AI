965d505ea8881bffc13a827fcd5bb119f0e0b56f
Roger Filomeno <rpfilomeno@gmail.com>
2026-08-10 13:08:24 +0800

    added filter for *text*

--- src/utils/llm.py
+++ src/utils/llm.py
@@ -18,6 +18,9 @@ def filter_response(response: str) -> str:
     response = re.sub(r"\(.*?\)", "", response)
     response = re.sub(r"[()\[\]{}]", "", response)
     response = re.sub(r"[\U00010000-\U0010ffff]", "", response, flags=re.UNICODE)
+    response = re.sub(r"<.*?>", "", response)
+    # remove text like *sample*
+    response = re.sub(r"\*.*?\*", "", response)
     return response
 
 
